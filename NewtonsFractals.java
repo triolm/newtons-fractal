6e74060cda4f1433135ea3486edf0e3e27aed7b7
Marisa Triola <marisatriola@gmail.com>
2023-08-22 13:37:34 -0400

    hardcode default colors for newton's fractals

--- NewtonsFractals.java
+++ NewtonsFractals.java
@@ -1,8 +1,11 @@
 public class NewtonsFractals extends ImageGenerator {
     private RootPolynomial p;
+    private Color[] colors;
+      private int[] defaultColors = {0xEF476F, 0xFFD166, 0x06D6A0, 0x118AB2, 0x073B4C, 0xB4A0E5};
 
     public NewtonsFractals(ComplexNumber[] roots) {
         p = new RootPolynomial(roots);
+        colors = generateColors(roots.length);
         this.filename = "newton";
     }
 
@@ -18,6 +21,24 @@ public class NewtonsFractals extends ImageGenerator {
             n = n.subtract(p.evaluate(n).divide(p.derivative().evaluate(n)));
         }
         return n;
+    }
+
+    public Color[] generateColors(int nColors){
+        Color[] c = new Color[nColors];
+
+        for (int i = 0; i < nColors; i ++){
+            if(i < defaultColors.length){
+                c[i] = Color.fromARGB(defaultColors[i]);
+                continue;
+            }
+            c[i] = new Color(Math.random(), Math.random(), Math.random());
+        }
+
+        return c;
+    }
+
+    public Color getColor(double n, double highest) {
+        return colors[(int)n];
     }
 
 }
